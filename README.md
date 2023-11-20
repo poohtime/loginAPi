db494048acc62ee6c1a8928c8c505620d4950397
poohtime <138019702+poohtime@users.noreply.github.com>
2023-11-20 11:04:26 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# loginApi
+https://docs.google.com/spreadsheets/d/1boKxEvpjJXog0gzvUdDe61hnD7x271QCJ1ywSjIgDCQ/edit?usp=sharing
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// const connect = require("./models/index");
+// connect();
+const { sequelize } = require("./models");
+const cookieParser = require("cookie-parser");
+const productsRouter = require("./routes/products.router.js");
+const usersRouter = require("./routes/user.routes.js");
+const authRouter = require("./routes/auth.router.js");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api", [productsRouter, usersRouter, authRouter]);
+
+app.listen(port, () =>{
+    console.log(port, "포트로 서버가 열렸어요!");
+})
